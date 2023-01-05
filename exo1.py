77b8bb25889a6ef154a2cf51e5c94fafbb5d4246
Oriol Camps Pérez <oriol.camps-perez@univ-tlse3.fr>
2023-01-05 17:59:20 +0100

    FIN EXO 1

--- exo1.py
+++ exo1.py
@@ -84,7 +84,7 @@ def tests12():
 #tests12() # test avec partition6.txt
 
 # QUESTION 1.3
-def meilleur_voisin(X):
+def meilleur_voisin_simple(X):
     meilleur= X.copy()
     for i in range(len(X)):
         voisinX = X.copy()
@@ -96,8 +96,21 @@ def meilleur_voisin(X):
                 meilleur = voisinX
     return meilleur
 
+# QUESTION 1.8
+def meilleur_voisin_contrainte(X): # version modifiée pour la question 1.8
+    meilleur= X.copy()
+    for i in range(len(X)):
+        voisinX = X.copy()
+        voisinX[i] = 1 - voisinX[i]
+        if sum(voisinX) >= p and f(voisinX) < f(meilleur): ##
+            meilleur = voisinX
+        elif f(voisinX) == f(meilleur):
+            if random.randint(0, 1) == 1:
+                meilleur = voisinX
+    return meilleur
+
 # QUESTION 1.4
-def steepest_hill_climbing(X, max_depl=1000):
+def steepest_hill_climbing(X, max_depl=1000, meilleur_voisin=meilleur_voisin_simple):
     Xp = X.copy()
     nb_depl = 0
     stop = False
@@ -111,28 +124,76 @@ def steepest_hill_climbing(X, max_depl=1000):
     return Xp
 
 # QUESTION 1.5
-def steepest_hill_climbing_redemarrage(X, max_depl=1000, max_essais=1000):
+def steepest_hill_climbing_redemarrage(X, max_depl=1000, max_essais=1000, meilleur_voisin=meilleur_voisin_simple):
     nb_essais = 0
     Xp = X.copy()
     while nb_essais < max_essais:
-        mvX = steepest_hill_climbing(random_sol(), max_depl)
+        mvX = steepest_hill_climbing(random_sol(), max_depl, meilleur_voisin)
         if f(mvX) < f(Xp):
             Xp = mvX
         nb_essais += 1
     return Xp
 
-# QUESTION 1.6 (partie affichage)
+# QUESTION 1.7
+def recherche_tabou(X, taille, max_depl=1000, meilleur_voisin=meilleur_voisin_simple):
+    Xp = X.copy()
+    tabou = []
+    nb_depl = 0
+    stop = False
+    while nb_depl < max_depl and not stop:
+        mvX = meilleur_voisin(Xp)
+        if mvX in tabou:
+            stop = True
+        else:
+            if f(mvX) < f(Xp):
+                Xp = mvX
+            else:
+                stop = True
+            tabou.append(Xp)
+            if len(tabou) > taille:
+                tabou.pop(0)
+        nb_depl += 1
+    return Xp
+
+# AFFICHAGE
 print("n = "+str(n))
 print("p = "+str(p))
 print("Q = "+str(Q)+"\n")
-print("X de départ = \t\t\t"+str(randX)+"\t"+"f(X) = "+str(res12))
-shc_red= steepest_hill_climbing_redemarrage(randX)
-print("SHC avec redemarrage de X = \t"+str(shc_red)+"\t"+"f(X) = "+str(f(shc_red)))
+print("X de départ = \t\t\t\t\t"+str(randX)+"\t"+"f(X) = "+str(res12))
 
-# Résultats: 
-#   partition6.txt = -34 
+# affichage QUESTION 1.7
+rech_tabou_small= recherche_tabou(randX, 10)
+rech_tabou_big= recherche_tabou(randX, 1000)
+print("Recherche tabou (avec len(tabou)=10)=\t\t"+str(rech_tabou_small)+"\t"+"f(X) = "+str(f(rech_tabou_small)))
+print("Recherche tabou (avec len(tabou)=1000)=\t\t"+str(rech_tabou_big)+"\t"+"f(X) = "+str(f(rech_tabou_big)))
+# RÉSULTATS:
+#   partition6.txt :
+#       len(tabou)=10 :     -34, -29, -28 (3 resultats différents)
+#       len(tabou)=1000 :   -34, -29, -28 (3 resultats différents)
+#       observations : les résultats sont les mêmes dans les deux cas
+
+#   graphe12345.txt :
+#       len(tabou)=10 :     -20, -16 (2 resultats différents)
+#       len(tabou)=1000 :   -20, -16 (2 resultats différents)
+#       observations : les résultats sont parfois différents (alternance de -20 et -16)
+
+# affichage QUESTION 1.6
+shc_red= steepest_hill_climbing_redemarrage(randX)
+print("SHC avec redemarrage de X (sans contrainte)= \t"+str(shc_red)+"\t"+"f(X) = "+str(f(shc_red)))
+shc_red= steepest_hill_climbing_redemarrage(randX, meilleur_voisin=meilleur_voisin_contrainte)
+print("SHC avec redemarrage de X (avec contrainte)= \t"+str(shc_red)+"\t"+"f(X) = "+str(f(shc_red)))
+
+# Résultats sans contrainte: 
+#   partition6.txt =  -34 
+#   graphe12345.txt = -20
+# Résultats avec contrainte:
+#   partition6.txt =  -34
 #   graphe12345.txt = -20
 
+# Observations: l'algorithme de Steepest Hill Climbing avec redémarrage 
+#               est beaucoup beaucoup plus lent que l'algorithme de
+#               recherche tabou. Cependant, ses resultats sont plus
+#               stables et semblent plus proches de la solution optimale.
 
 # REFERENCES
 # https://www.freecodecamp.org/espanol/news/python-abre-archivo-como-leer-un-archivo-de-texto-linea-por-linea/
